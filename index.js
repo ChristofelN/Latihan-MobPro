69dad8bc4b4f8afe81995c9cf19ab57012ff6f1f
CRISTOFEL NAU <s11820005@student.unklab.ac.id>
2021-03-10 23:47:20 +0800

    basicofjavascript

---
+++ index.js
@@ -0,0 +1,22 @@
+let hello;
+hello = "hello world  !!!";
+
+const birtYear = 200;
+
+let age = 20;
+age += 2;
+let apakahSudahMenikah = false;
+let alamat;
+
+console.log("hello");
+console.log(birtYear);
+console.log(age);
+console.log(apakahSudahMenikah);
+console.log(alamat);
+
+let namaDepan ="Chris";
+let namaBelakang ="tofel";
+
+let namaLengkap = namaDepan + "" + namaBelakang;
+
+console.log(namaLengkap);
